eb8a09fdb7a86387671bb81cd9976217342c1c54
jgmchan <jgmchan@gmail.com>
2013-05-13 19:49:09 +1000

    Added the beginnings of a cli

--- lib/proteus/cli.rb
+++ lib/proteus/cli.rb
@@ -7,6 +7,15 @@ module Proteus
 
     desc "init", "Initialize a basic configuration"
     def init
+      #TODO:
+      # 1. Generate the config file if it's not there
+      # 2. Alias the git command  in the relevant shell to call proteus
+      #   e.g. alias git='proteus start || git $@'
+    end
+
+    desc "start", "Run proteus"
+    def start
+      Proteus::Execute.new.run!
     end
   end
 end
